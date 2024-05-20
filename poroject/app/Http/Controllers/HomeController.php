6e97ee4060d942a3720d74e3c8d8083ec8c830b9
shan <shanharsharathnayake@gmail.com>
2024-05-20 16:55:35 +0530

    Import html tamplate

--- poroject/app/Http/Controllers/HomeController.php
+++ poroject/app/Http/Controllers/HomeController.php
@@ -21,4 +21,11 @@ class HomeController extends Controller
             return redirect()->back();
         }
     }
+
+    public function index(){
+        return view('user.home');
+    }
+
+
+
 }
